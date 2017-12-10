4386c2c9ae41ef899333948649122838c567b46b
Ali Neer <ali@alineer.com>
2017-12-10 12:28:46 -0800

    set up for rainbow text

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'text/index'
+
+  root 'text#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
