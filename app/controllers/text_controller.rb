3cd0440039359851030ba4e00ae9a84d6354f98b
Ali Neer <ali@alineer.com>
2017-12-11 08:42:55 -0800

    controller done

--- app/controllers/text_controller.rb
+++ app/controllers/text_controller.rb
@@ -1,4 +1,23 @@
 class TextController < ApplicationController
+  
   def index
+    @texts = Text.all
   end
+
+  def new
+    @text = Text.new
+  end
+
+  def create
+    @text = Text.new(params[:text])
+    if @text.save
+      redirect_to @text
+    else
+      render "new"
+  end
+
+  def show
+    @text = Text.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
 	resources :text
-  get 'text/index'
-
+  
   root 'text#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
